a9e334d73c3f6540ad809aa7f67fdd4bf94021f8
Nicolas Guanuna <nguanuna@contesta.es>
2024-05-30 14:47:32 +0200

    prueba https

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import https from 'https' // Importa el módulo https
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    proxy: {
+      '/CodesnapBackend': {
+        target: 'https://54.205.134.77',
+        secure: false, // Esto ignorará los errores de certificados SSL
+        changeOrigin: true,
+        agent: new https.Agent({  
+          rejectUnauthorized: false, // Esto ignorará los certificados no autorizados
+        }),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
